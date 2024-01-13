def174147447cb4eaf493b46937cd1cc949f38a9
JunHo Seo <junhoseo@korea.ac.kr>
2024-01-13 09:41:13 +0000

    build: Finalize DevContainer setting.

--- .devcontainer/postCreateCommands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install APT packages.
-apt-get update && apt-get install -y cmake
-
-# Install PIP packages.
-pip install -U pip && \
-pip install wheel==0.38.4 setuptools==65.5.0 && # Workaround for old OpenAI Gym. \ 
-pip install -r requirements.txt
